36e661a24e23ef5eccd8b62a923a040debde24b8
Hrexandro <marcin_nowak_hrx@o2.pl>
2023-05-15 21:27:08 +0200

    add find method

--- binarySearchTrees.js
+++ binarySearchTrees.js
@@ -1,42 +1,3 @@
-//Build a Node class / factory. It should have an attribute for the data it stores as well as its left and right children.
-
-function Node (data, left, right){
-  return {
-    data,
-    left,
-    right
-  };
-}
-
-//Build a Tree class / factory which accepts an array when initialized.
-//The Tree class should have a root attribute which uses the return value of buildTree which you’ll write next.
-
-function Tree (arr){
-  return {
-    root: buildTree(arr)
-  };
-}
-
-
-//Write a buildTree function which takes an array of data
-//(e.g. [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-//and turns it into a balanced binary tree full of Node objects appropriately placed
-//(don’t forget to sort and remove duplicates!). The buildTree function should return the level-0 root node.
-function buildTree (arr){
-  if (arr.length === 0){
-    return null
-  }
-  let sortedArrWithoutDuplicates = mergeSort(arr).filter((value, index, array) => {return array.indexOf(value) === index})
-  let midPoint = parseInt((sortedArrWithoutDuplicates.length - 1) / 2)
-  let rootData = sortedArrWithoutDuplicates[midPoint]
-  let left = buildTree(sortedArrWithoutDuplicates.slice(0, midPoint))
-  let right = buildTree(sortedArrWithoutDuplicates.slice(midPoint + 1))
-  let root = Node(rootData, left, right)
-
-  return root
-}
-
-
 function mergeSort(array){
   if (array.length < 2){
       return array
@@ -76,7 +37,67 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
   }
 }
 
+//Build a Node class / factory. It should have an attribute for the data it stores as well as its left and right children.
+
+function Node (data, left, right){
+  return {
+    data,
+    left,
+    right
+  };
+}
+
+//Build a Tree class / factory which accepts an array when initialized.
+//The Tree class should have a root attribute which uses the return value of buildTree which you’ll write next.
+
+function Tree (arr){
+  return {
+    root: buildTree(arr),
+    find: function(value, currentNode = this.root){
+      if (currentNode === null){
+        return null
+      } else if (currentNode.data === value){
+        return currentNode
+      } else if (currentNode.left || currentNode.right){
+        let result = this.find(value, currentNode.left)
+        if (result === null){
+          result = this.find(value, currentNode.right)
+        }
+        return result
+      }
+      return null
+    }
+  };
+}
+
+// 
+
+
+//Write a buildTree function which takes an array of data
+//(e.g. [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+//and turns it into a balanced binary tree full of Node objects appropriately placed
+//(don’t forget to sort and remove duplicates!). The buildTree function should return the level-0 root node.
+function buildTree (arr){
+  if (arr.length === 0){
+    return null
+  }
+  let sortedArrWithoutDuplicates = mergeSort(arr).filter((value, index, array) => {return array.indexOf(value) === index})
+  let midPoint = parseInt((sortedArrWithoutDuplicates.length - 1) / 2)
+  let rootData = sortedArrWithoutDuplicates[midPoint]
+  let left = buildTree(sortedArrWithoutDuplicates.slice(0, midPoint))
+  let right = buildTree(sortedArrWithoutDuplicates.slice(midPoint + 1))
+  let root = Node(rootData, left, right)
+
+  return root
+}
+
+
+
+
 let exampleArray = [10, 14, 803, 1409, 937, 10743, 2, 702, 103, 81, 36, 7]
+let exampleTree = Tree(exampleArray)
+// prettyPrint(buildTree(exampleArray))
+prettyPrint(exampleTree.root)
+// //console.log(buildTree(exampleArray))
 
-
-prettyPrint(buildTree(exampleArray))
+console.log(exampleTree.find(4))
